a009d9b6a9132f00940b218df6f5a0685911ca6c
전우성 <wjsdbwls5555@gmail.com>
2020-05-06 17:58:08 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$number = 5;
+
+switch($number){
+    case 1:
+        echo $number;
+    break;
+    case 2:
+        echo $number;
+    break;
+    default:
+        echo $number + 100;
+
+    
+
+
+}
+
+
+?>
